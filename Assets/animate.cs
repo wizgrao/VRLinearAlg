d1b7f061e0d2f6baf1828eb52edd01fff9881f23
Kevin Zhang <kevinzhang@airbears2-10-142-166-107.airbears2.1918.berkeley.edu>
2018-05-07 16:54:41 -0700

    project

---
+++ Assets/animate.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animate : MonoBehaviour {
+
+	bool animation;
+	bool finished;
+	float finalLeft;
+	float finalScale;
+	float del;
+	int counter;
+	// Use this for initialization
+	void Start () {
+		del = 200;
+		animation = false;
+		counter = 0;
+		finished = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (animation && (counter < ((int) del)) && !finished) {
+			transform.Rotate (0f, 0f, .1f);
+			transform.localScale += new Vector3 (0f, 0.002f, 0f);
+			Debug.Log (counter);
+			if (counter == 200) {
+				finished = true;
+			}
+		}
+		if (finished && counter > 0) {
+			transform.Rotate (0f, 0f, -0.1f);
+			transform.localScale -= new Vector3 (0f, 0.002f, 0f);
+			counter -= 1;
+			if (counter == 0) {
+				finished = false;
+			}
+		}
+
+//		transform.Rotate (10f, 10f, 10f);
+	}
+
+	public void setAnimation () {
+		animation = true;
+	}
+}
---
+++ Assets/changeImage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeImage : MonoBehaviour {
+
+	public Sprite text1;
+	public Sprite text2;
+	public Sprite text3;
+	public Sprite text4;
+	public Sprite text5;
+	public GameObject targetVec;
+	Image image;
+	int counter;
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<Image> ();
+		counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void transition () {
+		if (counter == 0) {
+			image.sprite = text2;
+			counter = 1;
+		} else if (counter == 1) {
+			image.sprite = text3;
+			counter = 2;
+		} else if (counter == 2) {
+			image.sprite = text4;
+			counter = 3;
+		} else if (counter == 3) {
+			image.sprite = text5;
+			counter = 4;
+		} else if (counter == 4) {
+			image.sprite = text1;
+			counter = 0;
+		}
+	}
+}
