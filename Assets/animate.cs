93cdd301ec74ec3e36ff191560a6abe69886ce36
Kevin Zhang <kevinzhang@airbears2-10-142-166-107.airbears2.1918.berkeley.edu>
2018-05-07 17:03:31 -0700

    asdf

--- Assets/animate.cs
+++ Assets/animate.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class animate : MonoBehaviour {
 
 	bool animation;
+	bool animation2;
 	bool finished;
 	float finalLeft;
 	float finalScale;
@@ -14,6 +15,7 @@ public class animate : MonoBehaviour {
 	void Start () {
 		del = 200;
 		animation = false;
+		animation2 = false;
 		counter = 0;
 		finished = false;
 	}
@@ -28,14 +30,14 @@ public class animate : MonoBehaviour {
 				finished = true;
 			}
 		}
-		if (finished && counter > 0) {
+/*		if (finished && counter > 0) {
 			transform.Rotate (0f, 0f, -0.1f);
 			transform.localScale -= new Vector3 (0f, 0.002f, 0f);
 			counter -= 1;
 			if (counter == 0) {
 				finished = false;
 			}
-		}
+		}*/
 
 //		transform.Rotate (10f, 10f, 10f);
 	}
